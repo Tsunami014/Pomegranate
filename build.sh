a25cd24fc0943cc01f4b558da16e940f337aebe4
Tsunami014 <tsunami014@duck.com>
2026-08-29 22:08:21 +1000

    Improved some output paths to hopefully make it work more consistently

--- build.sh
+++ build.sh
@@ -8,14 +8,12 @@ for dir in ./main ./pseudocode; do
     cd "$dir"
     echo "$dir"
     mkdir -p build
-    cat main.js | minify --type js -o build/main.js
-    minify style.css -o build/style.css
+    cat main.js | minify --type js -o main.min.js
+    minify style.css -o style.min.css
     if [[ "$dir" = "./main" ]]; then
-        minify debug.html -o ../index.html
-        sed -i 's|\./|./main/build/|g' ../index.html
+        minify main.html -o ../index.html
     else
-        minify debug.html -o index.html
-        sed -i 's|\./|./build/|g' index.html
+        minify main.html -o index.html
     fi
     cd - >/dev/null
 done
